1363e439367ab3cb7f8bb6e3d0b81d00d61bbac7
Matthew Minton <minton@posteo.net>
2020-06-08 10:24:29 -0400

    reset $env for import.py

--- import.py
+++ import.py
@@ -1,6 +1,13 @@
 import csv
+import os
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+
+engine = create_engine(os.getenv("DATABASE_URL"))
+db = scoped_session(sessionmaker(bind=engine))
 
 with open('books.csv') as csvfile:
     row = csv.reader(csvfile)
     for r in row:
-        print(r)
+        print(r[0])
